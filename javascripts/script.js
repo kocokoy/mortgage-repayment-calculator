28d2c95b442492ed78d0419091b7ebb9ce8ba1ad
kocokoy <vanneryckoconstantino@gmail.com>
2024-10-30 10:56:25 +0800

    fixed the hover state of the calculate button and focus state of radio buttons. Added functions to convert the string to int and to calculate the repayment

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,47 +5,70 @@ const mortgageInterestElement = document.querySelector('.jsMortgageInterest');
 // radio buttons
 const repaymentRadioElement = document.querySelector('.jsRepaymentRadio');
 const interestRadioElement = document.querySelector('.jsRepaymentInterest');
+let totalRepayment = 0;
+let monthlyPayment = 0;
 
-
+MortgageTypeBtnClicked();
 calculateButton.addEventListener('click', () => {
-  console.log(mortgageAmountElement.value);
-  console.log(mortgageTermElement.value);
-  console.log(mortgageInterestElement.value);
+  const mortgageAmount =  IntegerConverter(mortgageAmountElement.value);
+  const mortgageTerm =  IntegerConverter(mortgageTermElement.value);
+  const mortgageInterest =  IntegerConverter(mortgageInterestElement.value);
+  CalcuteRepayment(mortgageAmount,mortgageTerm,mortgageInterest);
 
-})
-
-let repaymentRadioClicked = false;
-let interestRadioClicked = false;
-
-const repaymentParentLabel = repaymentRadioElement.parentElement;
-const interestParentLabel = interestRadioElement.parentElement;
-
-repaymentRadioElement.addEventListener('click', () =>{
-  if(repaymentRadioClicked){
-    repaymentParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
-    
-    repaymentRadioElement.checked = false;
-    repaymentRadioClicked = false;
-  }else{
-    repaymentRadioClicked = true;
-    repaymentParentLabel.classList.add('mortgage-calculator-type-radio-clicked');
-    interestParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
-    interestRadioElement.checked = false;
-    interestRadioClicked = false;
-  }
 });
 
-interestRadioElement.addEventListener('click', () =>{
-  if(interestRadioClicked){
-    interestParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
-    interestRadioElement.checked = false;
-    interestRadioClicked = false;
-  }else{
-    repaymentParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
-    interestParentLabel.classList.add('mortgage-calculator-type-radio-clicked');
-    interestRadioClicked = true;
-    repaymentRadioElement.checked = false;
-    repaymentRadioClicked = false;
-  }
-});
+
+
+function MortgageTypeBtnClicked(){
+  let repaymentRadioClicked = false;
+  let interestRadioClicked = false;
+  
+  const repaymentParentLabel = repaymentRadioElement.parentElement;
+  const interestParentLabel = interestRadioElement.parentElement;
+  
+  repaymentRadioElement.addEventListener('click', () =>{
+    if(repaymentRadioClicked){
+      repaymentParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
+      repaymentRadioElement.checked = false;
+      repaymentRadioClicked = false;
+    }else{
+      repaymentRadioClicked = true;
+      repaymentParentLabel.classList.add('mortgage-calculator-type-radio-clicked');
+      interestParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
+      interestRadioElement.checked = false;
+      interestRadioClicked = false;
+    }
+  });
+  
+  interestRadioElement.addEventListener('click', () =>{
+    if(interestRadioClicked){
+      interestParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
+      interestRadioElement.checked = false;
+      interestRadioClicked = false;
+    }else{
+      repaymentParentLabel.classList.remove('mortgage-calculator-type-radio-clicked');
+      interestParentLabel.classList.add('mortgage-calculator-type-radio-clicked');
+      interestRadioClicked = true;
+      repaymentRadioElement.checked = false;
+      repaymentRadioClicked = false;
+    }
+  });
+};
+
+function IntegerConverter(str){
+  if(str.includes('.')){
+    return parseFloat(str);
+  };
+    return parseInt(str.replace(/,/g, ''));
+}
+
+function CalcuteRepayment(amount,years,interest){
+  let months = 12;
+  let monthlyInterestRate = ((interest/months)/100);
+  let totalPayments = (years*months);
+  let numerator = (amount * monthlyInterestRate);
+  let denominator = (1 - Math.pow(1 + monthlyInterestRate,(-1*totalPayments)));
+  monthlyPayment = (numerator/denominator);
+  totalRepayment =  (monthlyPayment*totalPayments);
+}
 
